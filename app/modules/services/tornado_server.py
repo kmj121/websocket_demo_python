8a685af9ae04082d881e5fc9d47259748de61000
kanmeijie@17zhiliao.com <kanmeijie@17zhiliao.com>
2021-05-28 16:02:12 +0800

    fix

--- app/modules/services/tornado_server.py
+++ app/modules/services/tornado_server.py
@@ -3,7 +3,7 @@ import tornado.web
 import tornado.websocket
 
 
-class ProStatus():
+class ProStatus:
     connector = {}  # 记录当前连接的user
 
     def user_connect(self, user):
@@ -14,7 +14,7 @@ class ProStatus():
         self.connector.pop(user)
 
     def trigger(self, message):
-        ''' 向所有被记录的客户端推送最新内容 '''
+        """ 向所有被记录的客户端推送最新内容 """
         for user in self.connector:
             user.write_message(message)
 
@@ -22,25 +22,25 @@ class ProStatus():
 class ReceiveHandler(tornado.web.RequestHandler):
     def get(self):
         msg = self.get_argument('msg', '')
-        ProStatus().trigger(msg) # 接收到消息之后推送
+        ProStatus().trigger(msg)  # 接收到消息之后推送
 
 
 class ConnectHandler(tornado.websocket.WebSocketHandler):
     def check_origin(self, origin):
-        '''重写同源检查 解决跨域问题'''
+        """重写同源检查 解决跨域问题"""
         return True
 
     def open(self):
-        '''新的websocket连接后被调动'''
-        ProStatus().user_connect(self) #用户连接后记录
+        """新的websocket连接后被调动"""
+        ProStatus().user_connect(self)  # 用户连接后记录
         self.write_message('Welcome')
 
     def on_close(self):
-        '''websocket连接关闭后被调用'''
+        """websocket连接关闭后被调用"""
         ProStatus().user_remove(self)  # 断开连接后remove
 
     def on_message(self, message):
-        '''接收到客户端消息时被调用'''
+        """接收到客户端消息时被调用"""
         self.write_message('new message :' + message)
 
 
